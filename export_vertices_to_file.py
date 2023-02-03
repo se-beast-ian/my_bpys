98ffffcaa1090f4c06a13a47688057900305fa68
seb-redbeard <sebastjan.rijavec@gmail.com>
2023-02-03 22:46:01 +0100

    Update export_vertices_to_file.py
    Somehow it works...

--- export_vertices_to_file.py
+++ export_vertices_to_file.py
@@ -1,12 +1,44 @@
 import bpy
 
-# Selected Object
-collider = bpy.context.object
 
-# Collect data 
-me = collider.data
+# js's  
+imps = ('import { Vector3 } from "three"')
+exp_imps = ('export default cylColliders')
+arr = ('const cylColliders = [')
+arr_e = (']')
+vec = ('new Vector3(')
+vec_e = ('),')
+vec_ef = (')') 
 
-# For each vertex in the range of vertices, print the coordinates
-for v in range(len(me.vertices)): 
-    print(me.vertices[v].co)
+f = open("c:\\dev\\colliders.js", "w")
+f.writelines(imps + "\n")
+f.writelines(arr + "\n")
+collider_geo = bpy.context.object
+
+me = collider_geo.data
+for v in range(len(me.vertices)):  
+    vertices = me.vertices[v].co
+    # assemble a string
+    vposs = (str(vertices.x) + ',' + str(vertices.y) + ',' + str(vertices.z))
+    
+    
+    if (v == len(me.vertices) - 1):
+        f.writelines(vec + vposs + vec_ef + "\n")
+    else: 
+        f.writelines(vec + vposs + vec_e + "\n")
+    
+f.writelines(arr_e + "\n")
+f.writelines(exp_imps + "\n")   
+f.close()
+
+ 
+    
+    
+'''
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+'''
   
+
+# new Vector3(3,0,0)
